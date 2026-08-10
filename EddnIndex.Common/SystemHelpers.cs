80db6f0197d974d432442b22de5e0b28e05e2c9d
Benjamin Peddell <klightspeed@killerwolves.net>
2026-08-11 08:51:51 +1000

    Make system name funcs more reusable

--- EddnIndex.Common/SystemHelpers.cs
+++ EddnIndex.Common/SystemHelpers.cs
@@ -9,9 +9,20 @@ namespace EddnIndex.Common
         {
             if (modSystemAddress is not long msa || msa < 0) return null;
 
-            var masscode = (msa >> 37) & 7;
-            var mid = (msa >> 16) & 0x1FFFFF;
+            var masscode = (int)((msa >> 37) & 7);
+            var mid = (int)((msa >> 16) & 0x1FFFFF);
             var n2 = msa & 0xFFFF;
+
+            return GetPGSuffix(masscode, mid, n2, includeN2);
+        }
+
+        public static string GetPGSuffix(int masscode, int mid, long n2, bool includeN2 = true)
+        {
+            ArgumentOutOfRangeException.ThrowIfLessThan(masscode, 0);
+            ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(masscode, 8);
+            ArgumentOutOfRangeException.ThrowIfLessThan(mid, 0);
+            ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(mid, 1 << 21);
+            ArgumentOutOfRangeException.ThrowIfLessThan(n2, 0);
 
             return string.Concat(
                 ' ',
@@ -96,6 +107,35 @@ namespace EddnIndex.Common
             return false;
         }
 
+        public static void SplitSystemAddress(long sysaddr, out int masscode, out int mid, out int sectorAddr, out long n2, out int bodyId)
+        {
+            bodyId = (int)(sysaddr >> 55) & 0x1FF;
+
+            sysaddr &= 0x007F_FFFF_FFFF_FFFF;
+
+            masscode = (int)(sysaddr & 7);
+            var zv = (sysaddr >> 3) & (0x3FFF >> masscode);
+            var yv = (sysaddr >> (17 - masscode)) & (0x1FFF >> masscode);
+            var xv = (sysaddr >> (30 - masscode * 2)) & (0x3FFF >> masscode);
+            n2 = sysaddr >> (44 - masscode * 3);
+            mid = (int)((xv & (0x7F >> masscode)) | ((yv & (0x7F >> masscode)) << 7) | ((zv & (0x7F >> masscode)) << 14));
+            sectorAddr = (int)((xv >> (7 - masscode)) | ((yv >> (7 - masscode)) << 7) | ((zv >> (7 - masscode)) << 13));
+        }
+
+        public static long MergeSystemAddress(int masscode, int mid, int sectorAddr, long n2, int bodyId)
+        {
+            long xv = ((mid >> 0) & 0x7F) + (((sectorAddr >> 0) & 0x7F) << (7 - masscode));
+            long yv = ((mid >> 7) & 0x7F) + (((sectorAddr >> 7) & 0x3F) << (7 - masscode));
+            long zv = ((mid >> 14) & 0x7F) + (((sectorAddr >> 13) & 0x7F) << (7 - masscode));
+
+            return unchecked((long)bodyId << 55)
+                 | (long)n2 << (44 - masscode * 3)
+                 | (xv << (30 - masscode * 2))
+                 | (yv << (17 - masscode * 1))
+                 | (zv << (3 - masscode * 0))
+                 | (long)masscode;
+        }
+
         public static long? SystemAddressToModSystemAddress(long? systemAddress)
         {
             if (systemAddress is not long sysaddr || sysaddr < 0)
@@ -105,17 +145,11 @@ namespace EddnIndex.Common
 
             sysaddr &= 0x007F_FFFF_FFFF_FFFF;
 
-            var masscode = (int)(sysaddr & 7);
-            var zv = (sysaddr >> 3) & (0x3FFF >> masscode);
-            var yv = (sysaddr >> (17 - masscode)) & (0x1FFF >> masscode);
-            var xv = (sysaddr >> (30 - masscode * 2)) & (0x3FFF >> masscode);
-            var n2 = sysaddr >> (44 - masscode * 3);
-            var mid = (xv & (0x7F >> masscode)) | ((yv & (0x7F >> masscode)) << 7) | ((zv & (0x7F >> masscode)) << 14);
-            var sectorAddr = (xv >> (7 - masscode)) | ((yv >> (7 - masscode)) << 7) | ((zv >> (7 - masscode)) << 13);
+            SplitSystemAddress(sysaddr, out int masscode, out int mid, out int sectorAddr, out long n2, out _);
 
             if (n2 > 65536) return null;
 
-            return n2 | (mid << 16) | ((long)masscode << 37) | (sectorAddr << 40);
+            return n2 | ((long)mid << 16) | ((long)masscode << 37) | ((long)sectorAddr << 40);
         }
 
         public static long? ModSystemAddressToSystemAddress(long? modSystemAddress)
@@ -124,15 +158,9 @@ namespace EddnIndex.Common
 
             var masscode = (int)((msa >> 37) & 7);
             var n2 = msa & 0xFFFF;
-            var xv = ((msa >> 16) & 0x7F) + (((msa >> 40) & 0x7F) << (7 - masscode));
-            var yv = ((msa >> 23) & 0x7F) + (((msa >> 47) & 0x3F) << (7 - masscode));
-            var zv = ((msa >> 30) & 0x7F) + (((msa >> 53) & 0x7F) << (7 - masscode));
-
-            return n2 << (44 - masscode * 3)
-                 | (xv << (30 - masscode * 2))
-                 | (yv << (17 - masscode * 1))
-                 | (zv << (3 - masscode * 0))
-                 | (long)masscode;
+            var mid = (int)((msa >> 16) & 0x1FFFFF);
+            var sectorAddr = (int)(msa >> 40);
+            return MergeSystemAddress(masscode, mid, sectorAddr, n2, 0);
         }
     }
 }
